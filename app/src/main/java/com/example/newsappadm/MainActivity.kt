d04ce16aa114d231dec6f10dd03dfc244ba85807
EnriqueIzel2 <eleaoizel@gmail.com>
2022-06-16 11:44:12 -0400

    feat: update the main activity

--- app/src/main/java/com/example/newsappadm/MainActivity.kt
+++ app/src/main/java/com/example/newsappadm/MainActivity.kt
@@ -1,57 +1,28 @@
 package com.example.newsappadm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import com.example.newsappadm.databinding.ActivityMainBinding
-import com.google.firebase.firestore.FirebaseFirestore
+
 
 class MainActivity : AppCompatActivity() {
-
   private lateinit var binding: ActivityMainBinding
-  private val db = FirebaseFirestore.getInstance()
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    supportActionBar!!.hide()
+
     binding = ActivityMainBinding.inflate(layoutInflater)
     setContentView(binding.root)
 
-    supportActionBar!!.hide()
-
-    binding.btnPublicarNoticia.setOnClickListener {
-      val titulo = binding.editTituloNoticia.text.toString()
-      val noticia = binding.editNoticia.text.toString()
-      val data = binding.editDataNoticia.text.toString()
-      val autor = binding.editAutorNoticia.text.toString()
-
-      if (titulo.isEmpty() || noticia.isEmpty() || data.isEmpty() || autor.isEmpty()) {
-        Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show()
-      } else {
-        salvarNoticia(titulo, noticia, data, autor)
-      }
+    binding.btnNextScreen.setOnClickListener {
+      goToNextScreen()
     }
   }
 
-  private fun salvarNoticia(titulo: String, noticia: String, data: String, autor: String) {
-    val mapNoticias = hashMapOf(
-      "titulo" to titulo,
-      "noticia" to noticia,
-      "data" to data,
-      "autor" to autor
-    )
-
-    db.collection("noticias").add(mapNoticias).addOnCompleteListener { tarefa ->
-      if (tarefa.isSuccessful) {
-        Toast.makeText(this, "Notícia salva com sucesso", Toast.LENGTH_SHORT).show()
-        limparCampos()
-      }
-    }
-  }
-
-  private fun limparCampos() {
-    binding.editTituloNoticia.setText("")
-    binding.editNoticia.setText("")
-    binding.editDataNoticia.setText("")
-    binding.editAutorNoticia.setText("")
+  private fun goToNextScreen() {
+    val nextScreen = Intent(this, CreateNews::class.java)
+    startActivity(nextScreen)
   }
 }
